d01d14b8b5b5c290d03783b27067950d3e58c972
monikabaabu <monikababu207@gmail.com>
2025-08-13 14:29:08 +0530

    .

---
+++ problems/recursion/NumberofStepstoReduceaNumbertoZero.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int steps(int n, int c ){
+        if(n==0){
+            return c;
+        }
+        if(n%2==0){
+            return steps(n/2,c+1);
+        }
+        return steps(n-1,c+1);
+    }
+    public int numberOfSteps(int num) {
+        return steps(num,0);
+    }
+}
