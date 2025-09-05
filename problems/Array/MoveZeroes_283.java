0ec335f0e82aa5e33090e35926239c682063b854
monikabaabu <monikababu207@gmail.com>
2025-09-05 11:30:47 +0530

    79

---
+++ problems/Array/MoveZeroes_283.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int size=nums.length;
+        int count=0;
+        for(int i=0; i<size; i++){
+            if(nums[i]!=0){
+                nums[count]=nums[i];
+                count++;
+            }
+        }
+        for( int j= count; j<size; j++){
+            nums[j]=0;
+        }
+    }
+}
