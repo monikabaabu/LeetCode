7b860d35b809f511ead6204766868e1bd637d74f
monikabaabu <monikababu207@gmail.com>
2025-10-02 20:07:21 +0530

    medium

---
+++ problems/SlidingWindow/PermutationinString_567.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] s1count = new int[26];
+        int[] s2count = new int[26];
+        for(char c: s1.toCharArray()){
+            s1count[c-'a']++;
+        }
+        for(int i=0;i<s2.length();i++){
+            s2count[s2.charAt(i)-'a']++;
+            if(i>=s1.length()){
+                s2count[s2.charAt(i-s1.length())-'a']--;
+            }
+            if(Arrays.equals(s1count,s2count)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
